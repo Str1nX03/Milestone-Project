4e7f417bad662434f9d9bb9ce55ca4a626a0bf6b
Str1nX03 <draveen2210@gmail.com>
2025-01-25 18:36:36 +0530

    Second Commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 HYPHEN_E_DOT = "-e ."
 def get_requirements(file_path:str)->List[str]:
     """
-    This function wqill return the list of requirements
+    This function will return the list of requirements
     """
     requirements = []
     with open(file_path) as file_obj:
