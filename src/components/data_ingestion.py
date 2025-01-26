6dc9663a35d6b5373b9df54239479b1336d1e26f
Str1nX03 <draveen2210@gmail.com>
2025-01-26 18:33:20 +0530

    data transformation is implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,14 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts" , "train.csv")
     test_data_path: str = os.path.join("artifacts" , "test.csv")
-    raw_data_path: str = os.path.join("artifacts" , "raw.csv")
+    data_path: str = os.path.join("artifacts" , "data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -23,7 +25,7 @@ class DataIngestion:
 
             logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
-            df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
+            df.to_csv(self.ingestion_config.data_path, index = False, header = True)
 
             logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size = 0.2, random_state = 42)
@@ -41,4 +43,6 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
